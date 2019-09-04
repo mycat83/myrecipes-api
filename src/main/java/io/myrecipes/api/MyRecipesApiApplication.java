7bba7cecc0b0395142d4fbb6976c96d95e9fad85
Jeongsu Yang <mycat83@gmail.com>
2019-09-04 23:56:16 +0900

    기능: 프로젝트 초기 설정
    - @RestController 사용을 위해서 pom.xml Dependency 변경
    (spring-boot-starter -> spring-boot-starter-web)
    - tomcat 포트 설정을 위해서 application.yml 수정
    - 서비스 상태 확인을 위한 HealthController 생성 및 관련 단위테스트 생성
    
    해결: #1

--- src/main/java/io/myrecipes/api/MyRecipesApiApplication.java
+++ src/main/java/io/myrecipes/api/MyRecipesApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyrecipesApiApplication {
+public class MyRecipesApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyrecipesApiApplication.class, args);
+        SpringApplication.run(MyRecipesApiApplication.class, args);
     }
 
 }
---
+++ src/test/java/io/myrecipes/api/MyRecipesApiApplicationTests.java
@@ -0,0 +1,46 @@
+package io.myrecipes.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        properties = {
+                "value=test",
+                "property.value=propertyTest"
+        },
+        classes = {MyRecipesApiApplicationTests.class}
+)
+public class MyRecipesApiApplicationTests {
+
+    @Autowired
+    DefaultListableBeanFactory beanFactory;
+
+    @Value("${value}")
+    private String value;
+
+    @Value("${property.value}")
+    private String propertyValue;
+
+    @Test
+    public void contextLoads() {
+        assertThat(value, is("test"));
+        assertThat(propertyValue, is("propertyTest"));
+    }
+
+    @Test
+    public void beans() {
+        for (String bean: beanFactory.getBeanDefinitionNames()) {
+            System.out.println(bean + " \t" + beanFactory.getBean(bean).getClass().getName());
+        }
+    }
+
+}
