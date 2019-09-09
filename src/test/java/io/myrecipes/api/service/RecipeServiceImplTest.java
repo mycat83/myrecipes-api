43c4c520d1a35cf7e1aceb7190df4dc4250a6b14
Jeongsu Yang <mycat83@gmail.com>
2019-09-09 22:18:41 +0900

    기능: 레시피 저장/수정/삭제 API 추가(계속)
    * 레시피 관련 service 단위테스트 작성
    
    해결: #3

--- src/test/java/io/myrecipes/api/service/RecipeServiceImplTest.java
+++ src/test/java/io/myrecipes/api/service/RecipeServiceImplTest.java
@@ -2,7 +2,6 @@ package io.myrecipes.api.service;
 
 import io.myrecipes.api.domain.Recipe;
 import io.myrecipes.api.repository.RecipeRepository;
-import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,10 +9,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -36,16 +37,54 @@ public class RecipeServiceImplTest {
     }
 
     @Test
-    public void Should_동일한_값_반환_When_저장_성공() {
+    public void Should_동일한_항목_반환_When_저장_성공() {
         given(recipeRepository.save(recipe1)).willReturn(recipe1);
 
-        final Recipe savedRecipe1 = recipeService.createRecipe(recipe1);
+        final Recipe savedRecipe = recipeService.createRecipe(recipe1);
 
-        assertThat(savedRecipe1, not(nullValue()));
-        assertThat(savedRecipe1.getTitle(), equalTo(recipe1.getTitle()));
-        assertThat(savedRecipe1.getImage(), equalTo(recipe1.getImage()));
-        assertThat(savedRecipe1.getEstimatedTime(), equalTo(recipe1.getEstimatedTime()));
-        assertThat(savedRecipe1.getDifficulty(), equalTo(recipe1.getDifficulty()));
+        assertThat(savedRecipe, not(nullValue()));
+        assertThat(savedRecipe.getTitle(), equalTo(recipe1.getTitle()));
+        assertThat(savedRecipe.getImage(), equalTo(recipe1.getImage()));
+        assertThat(savedRecipe.getEstimatedTime(), equalTo(recipe1.getEstimatedTime()));
+        assertThat(savedRecipe.getDifficulty(), equalTo(recipe1.getDifficulty()));
     }
 
+    @Test
+    public void Should_리스트_반환_When_저장_성공() {
+        List<Recipe> list = new ArrayList<>();
+        list.add(recipe1);
+        list.add(recipe2);
+        list.add(recipe3);
+        given(recipeRepository.findAll()).willReturn(list);
+
+        final List<Recipe> foundList = recipeService.readRecipeList();
+
+        assertThat(foundList.size(), is(3));
+        assertThat(foundList.get(0).getTitle(), is("test1"));
+        assertThat(foundList.get(1).getTitle(), is("test2"));
+        assertThat(foundList.get(2).getTitle(), is("test3"));
+    }
+
+    @Test
+    public void Should_업데이트된_항목_반환_When_업데이트_성공() {
+        given(recipeRepository.getOne(1)).willReturn(recipe1);
+        given(recipeRepository.save(recipe2)).willReturn(recipe2);
+
+        final Recipe updatedRecipe = recipeService.updateRecipe(1, recipe2);
+
+        assertThat(updatedRecipe, not(nullValue()));
+        assertThat(updatedRecipe.getTitle(), equalTo(recipe2.getTitle()));
+        assertThat(updatedRecipe.getImage(), equalTo(recipe2.getImage()));
+        assertThat(updatedRecipe.getEstimatedTime(), equalTo(recipe2.getEstimatedTime()));
+        assertThat(updatedRecipe.getDifficulty(), equalTo(recipe2.getDifficulty()));
+    }
+
+    @Test
+    public void Should_Null_반환_When_업데이트_실패() {
+        given(recipeRepository.getOne(1)).willReturn(null);
+
+        final Recipe updatedRecipe = recipeService.updateRecipe(1, recipe2);
+
+        assertThat(updatedRecipe, is(nullValue()));
+    }
 }
