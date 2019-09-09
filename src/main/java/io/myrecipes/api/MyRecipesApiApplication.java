1adfa9a193cec367d0a66c2ac8a8888fb21b4936
Jeongsu Yang <mycat83@gmail.com>
2019-09-09 22:32:23 +0900

    기타: MyBatis 설정 제거
    * MyBatis 관련 dependency, 설정 제거

--- src/main/java/io/myrecipes/api/MyRecipesApiApplication.java
+++ src/main/java/io/myrecipes/api/MyRecipesApiApplication.java
@@ -1,11 +1,9 @@
 package io.myrecipes.api;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "io.myrecipes.api.mapper")
 public class MyRecipesApiApplication {
 
     public static void main(String[] args) {
