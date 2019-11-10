20d36d0f1c5ceff088c38263be9725789bf7826f
Jeongsu Yang <mycat83@gmail.com>
2019-11-10 11:14:12 +0900

    기타: 패키지 참조 수정
    * 패키지 변경 시 누락된 부분 수정

--- src/main/java/link/myrecipes/api/config/SwaggerConfig.java
+++ src/main/java/link/myrecipes/api/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("io.myrecipes.api.controller"))
+                .apis(RequestHandlerSelectors.basePackage("link.myrecipes.api.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo())
