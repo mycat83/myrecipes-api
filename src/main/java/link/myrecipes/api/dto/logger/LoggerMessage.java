02037c2e19acec03435c57bc0ddeab9d4568a8c0
Jeongsu Yang <mycat83@gmail.com>
2019-11-25 18:20:27 +0900

    기능: 호출 로깅 구현
    * 로깅 정보에 시스템 정보 추가
    
    관련: #49

--- src/main/java/link/myrecipes/api/dto/logger/LoggerMessage.java
+++ src/main/java/link/myrecipes/api/dto/logger/LoggerMessage.java
@@ -11,6 +11,8 @@ import lombok.ToString;
 public class LoggerMessage {
     private String logType;
 
+    private String system;
+
     private String method;
 
     private String arguments;
@@ -22,8 +24,9 @@ public class LoggerMessage {
     private String registerDate;
 
     @Builder
-    public LoggerMessage(String logType, String method, String arguments, String exception, String exceptionMessage, String registerDate) {
+    public LoggerMessage(String logType, String system, String method, String arguments, String exception, String exceptionMessage, String registerDate) {
         this.logType = logType;
+        this.system = system;
         this.method = method;
         this.arguments = arguments;
         this.exception = exception;
