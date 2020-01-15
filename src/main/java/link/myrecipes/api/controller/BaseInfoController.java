d0122ebb598e337a5a63a531c27879366a3f7612
Jeongsu Yang <mycat83@gmail.com>
2020-01-16 00:20:42 +0900

    기능: 스프링 REST Docs 문서 적용
    - 재료 리스트 조회: self link 추가
    - link 추가 테스트 코드 작성중
    
    관련: #65

--- src/main/java/link/myrecipes/api/controller/BaseInfoController.java
+++ src/main/java/link/myrecipes/api/controller/BaseInfoController.java
@@ -5,15 +5,21 @@ import io.swagger.annotations.ApiOperation;
 import link.myrecipes.api.dto.Material;
 import link.myrecipes.api.dto.Unit;
 import link.myrecipes.api.service.BaseInfoService;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 @Api(tags = {"baseInfo"})
 @RestController
 public class BaseInfoController {
+
     private final BaseInfoService baseInfoService;
 
     public BaseInfoController(BaseInfoService baseInfoService) {
@@ -22,14 +28,21 @@ public class BaseInfoController {
 
     @GetMapping("/materials")
     @ApiOperation("재료 리스트 조회")
-    public ResponseEntity<List<Material>> readMaterialList() {
+    public ResponseEntity<Resources<Material>> readMaterialList() {
+
         List<Material> materialList = this.baseInfoService.readMaterialList();
-        return new ResponseEntity<>(materialList, HttpStatus.OK);
+
+        Resources<Material> materialResources = new Resources<>(materialList);
+        ControllerLinkBuilder selfLinkBuilder = linkTo(methodOn(BaseInfoController.class).readMaterialList());
+        materialResources.add(selfLinkBuilder.withSelfRel());
+
+        return ResponseEntity.ok(materialResources);
     }
 
     @PostMapping("/materials")
     @ApiOperation("재료 저장")
     public ResponseEntity<Material> createMaterial(@RequestBody Material material, @RequestParam int userId) {
+
         Material savedMaterial = this.baseInfoService.createMaterial(material, userId);
         return new ResponseEntity<>(savedMaterial, HttpStatus.OK);
     }
@@ -37,6 +50,7 @@ public class BaseInfoController {
     @PostMapping("/units")
     @ApiOperation("단위 저장")
     public ResponseEntity<Unit> createUnit(@RequestBody Unit unit, @RequestParam int userId) {
+
         Unit savedUnit = this.baseInfoService.createUnit(unit, userId);
         return new ResponseEntity<>(savedUnit, HttpStatus.OK);
     }
